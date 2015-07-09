749674852c0fc77551b60b6400ab2e8a5dbde980
Antoine Lafarge <ant.lafarge@gmail.com>
2015-07-09 15:04:54 +0200

    update android project for raknet

--- android_samples/raknet/jni/Android.mk
+++ android_samples/raknet/jni/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := raknet
-MY_PREFIX := $(LOCAL_PATH)/../src/
+MY_PREFIX := $(LOCAL_PATH)/../../../RakNet/Source/
 MY_SOURCES := $(wildcard $(MY_PREFIX)*.cpp)
-LOCAL_SRC_FILES += $(MY_SOURCES:$(MY_PREFIX)%=../src/%)
+LOCAL_SRC_FILES += $(MY_SOURCES:$(MY_PREFIX)%=../../../RakNet/Source/%)
 
 include $(BUILD_SHARED_LIBRARY)
