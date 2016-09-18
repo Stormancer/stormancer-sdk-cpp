c9a5aab03d4482c774df150aab65d539dec4b72f
Jean-Michel Deruty <jmderuty@stormancer.Com>
2016-09-18 12:59:50 +0200

    Added VS140 solution tu BuildAll.bat

--- BuildAll.bat
+++ BuildAll.bat
@@ -6,3 +6,11 @@ msbuild stormancer-sdk-cpp-120.sln /p:Configuration=Debug /p:Platform=x86
 
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "Write-NugetPackage .\stormancer-120-static.autopkg"
 
+.nuget\nuget.exe restore stormancer-sdk-cpp-140.sln
+msbuild stormancer-sdk-cpp-140.sln /p:Configuration=Release /p:Platform=x64
+msbuild stormancer-sdk-cpp-140.sln /p:Configuration=Release /p:Platform=x86
+msbuild stormancer-sdk-cpp-140.sln /p:Configuration=Debug /p:Platform=x64
+msbuild stormancer-sdk-cpp-140.sln /p:Configuration=Debug /p:Platform=x86
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Write-NugetPackage .\stormancer-140-static.autopkg"
+
