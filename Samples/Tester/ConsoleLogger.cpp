b017496dd4fb491e5994ae80638a7776712296a3
Antoine Lafarge <ant.lafarge@gmail.com>
2017-04-14 18:06:02 +0200

    fix console logger

--- Samples/Tester/ConsoleLogger.cpp
+++ Samples/Tester/ConsoleLogger.cpp
@@ -9,18 +9,18 @@ ConsoleLogger::~ConsoleLogger()
 {
 }
 
-void ConsoleLogger::log(const char* message)
+void ConsoleLogger::log(const std::string message)
 {
 	log(Stormancer::LogLevel::Info, "None", message, "");
 }
 
-void ConsoleLogger::log(Stormancer::LogLevel level, const char* category, const char* message, const char* data)
+void ConsoleLogger::log(Stormancer::LogLevel level, const std::string category, const std::string message, const std::string data)
 {
 	if (level > _maximalLogLevel)
 	{
 		return;
 	}
-	std::string message2 = format(level, category, message, data).get();
+	std::string message2 = format(level, category, message, data);
 
 	_mutex.lock();
 
--- Samples/Tester/ConsoleLogger.h
+++ Samples/Tester/ConsoleLogger.h
@@ -8,8 +8,8 @@ public:
 	virtual ~ConsoleLogger();
 
 public:
-	void log(const char* message);
-	void log(Stormancer::LogLevel level, const char* category, const char* message, const char* data);
+	void log(const std::string message);
+	void log(Stormancer::LogLevel level, const std::string category, const std::string message, const std::string data);
 	void log(const std::exception& e);
 
 public:
