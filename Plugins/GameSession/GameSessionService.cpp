30e9cf82983b3523fe1dc1af2ebae3617d060636
Stormancer-HAL9000 <hal9000@stormancer.com>
2018-03-09 16:33:46 +0100

    Origin: 3d2b330ff9338562b0005b007b26f08415286c4d
    This is an automated commit.

--- Plugins/GameSession/GameSessionService.cpp
+++ Plugins/GameSession/GameSessionService.cpp
@@ -4,54 +4,132 @@
 
 namespace Stormancer
 {
-	GameSessionService::GameSessionService(Scene_ptr scene)
+
+
+	GameSessionService::GameSessionService(Scene_ptr scene) :
+		_onShutdownReceived([]() {})
 	{
 		_scene = scene;
 
-		_scene->addRoute("server.started", [this](Packetisp_ptr packet) {
-			auto serverInfos = packet->readObject<GameServerInformations>();
-			this->_waitServerTce.set(serverInfos);
+		_logger = scene->dependencyResolver()->resolve<ILogger>();
+
+		_scene->addRoute("gameSession.shutdown", [this](Packetisp_ptr packet) {
+			if (this->_onShutdownReceived)
+			{
+				this->_onShutdownReceived();
+			}
 		});
-
 		_scene->addRoute("player.update", [this](Packetisp_ptr packet) {
-			auto update = packet->readObject<PlayerUpdate>();
-			SessionPlayer player{ update.UserId, (PlayerStatus)update.Status };
+			auto update = packet->readObject<Stormancer::PlayerUpdate>();
+			SessionPlayer player(update.UserId, (PlayerStatus)update.Status);
 
-			auto end = _users.end();
-			auto it = std::find_if(_users.begin(), end, [player](SessionPlayer p) { return p.PlayerId == player.PlayerId; });
+			auto end = this->_users.end();
+			auto it = std::find_if(this->_users.begin(), end, [player](SessionPlayer p) { return p.PlayerId == player.PlayerId; });
 			if (it == end)
 			{
-				_users.push_back(player);
+				this->_users.push_back(player);
 			}
 			else
 			{
 				*it = player;
 			}
-			_onConnectedPlayersChanged(player);
+			this->_onConnectedPlayersChanged(SessionPlayerUpdateArg(player, update.Data));
+		});
+
+		_scene->addRoute("player.p2ptoken", [this](Packetisp_ptr packet) {
+			auto p2pToken = packet->readObject<std::string>();
+			if (p2pToken.empty()) //host
+			{
+				_tunnel = _scene->registerP2PServer(GAMESESSION_P2P_SERVER_ID);
+
+				this->_onRoleReceived("HOST");
+			}
+			else //client
+			{
+				_scene->openP2PConnection(p2pToken).then([this](std::shared_ptr<Stormancer::P2PScenePeer> p2pPeer) {
+					if (_onConnectionOpened)
+					{
+						_onConnectionOpened(p2pPeer);
+					}
+					if (shouldEstablishTunnel)
+					{
+						return p2pPeer->openP2PTunnel(GAMESESSION_P2P_SERVER_ID).then([this](std::shared_ptr<Stormancer::P2PTunnel> guestTunnel)
+						{
+							if (_onTunnelOpened)
+							{
+								_onTunnelOpened(guestTunnel);
+							}
+
+							this->_onRoleReceived("CLIENT");
+						});
+					}
+					else
+					{
+						return pplx::task_from_result();
+					}
+				}).then([this](pplx::task<void> t) {
+					try
+					{
+						t.get();
+					}
+					catch (const std::exception& ex)
+					{
+						_logger->log(ex);
+					}
+				});
+			}
 		});
 	}
 
-	pplx::task<GameServerInformations> GameSessionService::waitServerReady(pplx::cancellation_token token)
+	pplx::task<std::string> GameSessionService::waitServerReady(pplx::cancellation_token token)
 	{
 		return pplx::create_task(_waitServerTce, pplx::task_options(token));
 	}
 
-	std::vector<SessionPlayer> GameSessionService::getConnectedPlayers()
+	std::vector<SessionPlayer> GameSessionService::GetConnectedPlayers()
 	{
-		return _users;
+		return this->_users;
 	}
 
-	void GameSessionService::unsubscribeConnectedPlayersChanged(Action<SessionPlayer>::TIterator handle)
+	void GameSessionService::unsubscribeConnectedPlayersChanged(Action<SessionPlayerUpdateArg>::TIterator handle)
 	{
-		_onConnectedPlayersChanged.erase(handle);
+		this->_onConnectedPlayersChanged.erase(handle);
 	}
 
-	std::function<void()> GameSessionService::onConnectedPlayerChanged(std::function<void(SessionPlayer)> callback)
+	std::function<void()> Stormancer::GameSessionService::OnConnectedPlayerChanged(std::function<void(SessionPlayer, std::string)> callback)
 	{
-		auto iterator = _onConnectedPlayersChanged.push_back(callback);
+		auto iterator = this->_onConnectedPlayersChanged.push_back([callback](SessionPlayerUpdateArg args) {callback(args.sessionPlayer, args.data); });
 		return [iterator, this]() {
 			unsubscribeConnectedPlayersChanged(iterator);
 		};
+	}
+
+	void GameSessionService::unsubscribeRoleReceived(Action < std::string > ::TIterator handle)
+	{
+		this->_onRoleReceived.erase(handle);
+	}
+
+	std::function<void()> Stormancer::GameSessionService::OnRoleReceived(std::function<void(std::string)> callback)
+	{
+		auto iterator = this->_onRoleReceived.push_back([callback](std::string role) {callback(role); });
+		return [iterator, this]() {
+			unsubscribeRoleReceived(iterator);
+		};
+	}
+
+	void GameSessionService::OnTunnelOpened(std::function<void(std::shared_ptr<Stormancer::P2PTunnel>)> callback)
+	{
+		_onTunnelOpened = callback;
+	}
+
+	void GameSessionService::OnP2PConnected(std::function<void(std::shared_ptr<Stormancer::P2PScenePeer>)> callback)
+	{
+		_onConnectionOpened = callback;
+	}
+
+	void GameSessionService::OnShutdownReceived(std::function<void(void)> callback)
+	{
+		_onShutdownReceived = callback;
 	}
 
 	pplx::task<void> GameSessionService::connect()
@@ -62,11 +140,53 @@ namespace Stormancer
 	pplx::task<void> GameSessionService::reset()
 	{
 		auto rpc = _scene->dependencyResolver()->resolve<RpcService>();
-		return rpc->rpcWriter("gamesession.reset", Writer());
+		return rpc->rpcWriter("gamesession.reset", [](obytestream* _) {});
 	}
 
-	void GameSessionService::ready()
+	pplx::task<void> GameSessionService::disconnect()
 	{
-		_scene->send("gamesession.playerready", Writer());
+		if (_scene)
+		{
+			return _scene->disconnect();
+		}
+		else
+		{
+			return pplx::task_from_result();
+		}
+	}
+
+	void GameSessionService::ready(std::string data)
+	{
+		_scene->send("player.ready", [data](obytestream* stream) {
+			msgpack::pack(stream, data);
+		});
+	}
+
+	GameSessionManager::GameSessionManager(Client* client) :client(client)
+	{
+
+	}
+
+	void GameSessionManager::setToken(std::string token)
+	{
+		this->currentGameSessionId = "";
+		this->token = token;
+
+	}
+
+	pplx::task<Stormancer::Scene_ptr> GameSessionManager::getCurrentGameSession()
+	{
+		if (this->currentGameSessionId.empty())
+		{
+			return this->client->getConnectedScene(this->currentGameSessionId);
+		}
+		else
+		{
+			return this->client->getPrivateScene(this->token).then([this](pplx::task<Scene_ptr> t) {
+				auto scene = t.get();
+				this->currentGameSessionId = scene->id();
+				return scene;
+			});
+		}
 	}
 }
--- Plugins/GameSession/GameSessionService.h
+++ Plugins/GameSession/GameSessionService.h
@@ -1,35 +1,49 @@
 #pragma once
 #include "stormancer/headers.h"
 #include "stormancer/Scene.h"
+#include "stormancer/Client.h"
 #include "stormancer/RPC/RpcService.h"
 
 namespace Stormancer
 {
-	struct GameServerInformations
-	{
-		std::string Ip;
-		int Port;
+	const std::string GAMESESSION_P2P_SERVER_ID = "GameSession";
 
-		MSGPACK_DEFINE(Ip, Port);
-	};
-
-	struct GameResults
+	struct SetResult
 	{
+	public:
 		std::string winnerId;
+
 		int winnerScore;
 		int loserScore;
 
 		MSGPACK_DEFINE(winnerId, winnerScore, loserScore);
 	};
 
-	struct MatchConsequences
+	struct GameResults
 	{
+	public:
+		std::string winnerId;
+
+		bool forfait;
+		std::vector<SetResult> sets;
+
+		MSGPACK_DEFINE(winnerId, forfait, sets);
+	};
+
+	struct MMRChanges
+	{
+	public:
 		std::string WinnerId;
 		int WinnerGain;
+
+
 		std::string LoserId;
 		int LoserGain;
 
-		MSGPACK_DEFINE(WinnerId, WinnerGain, LoserId, LoserGain);
+		std::string winnerNewLeague;
+		std::string loserNewLeague;
+
+		MSGPACK_DEFINE(WinnerId, WinnerGain, LoserId, LoserGain, winnerNewLeague, loserNewLeague);
 	};
 
 	enum class PlayerStatus
@@ -43,32 +57,73 @@ namespace Stormancer
 
 	struct SessionPlayer
 	{
+	public:
+		SessionPlayer(std::string playerId, PlayerStatus status)
+			: PlayerId(playerId), Status(status) {}
 		std::string PlayerId;
 		PlayerStatus Status;
 	};
 
+	struct ServerStartedMessage
+	{
+	public:
+		std::string p2pToken;
+		MSGPACK_DEFINE(p2pToken);
+	};
+
 	struct PlayerUpdate
 	{
+	public:
 		std::string UserId;
 		int Status;
-		std::string FaultReason;
+		std::string Data;
 
-		MSGPACK_DEFINE(UserId, Status, FaultReason);
+		MSGPACK_DEFINE(UserId, Status, Data);
+	};
+
+	class GameSessionManager
+	{
+	public:
+		GameSessionManager(Client* client);
+		std::string currentGameSessionId;
+
+		void setToken(std::string token);
+
+		pplx::task<Scene_ptr> getCurrentGameSession();
+
+	private:
+		std::string token;
+		Client* client;
 	};
 
 	class GameSessionService
 	{
+
+	private:
+		struct SessionPlayerUpdateArg
+		{
+		public:
+			SessionPlayerUpdateArg(SessionPlayer player, std::string data)
+				: sessionPlayer(player), data(data) {}
+
+			SessionPlayer sessionPlayer;
+			std::string data;
+		};
+
 	public:
+		GameSessionService(Stormancer::Scene_ptr scene);
 
-#pragma region public_methods
+		pplx::task<std::string> waitServerReady(pplx::cancellation_token);
 
-		GameSessionService(Scene_ptr scene);
+		std::vector<SessionPlayer> GetConnectedPlayers();
 
-		pplx::task<GameServerInformations> waitServerReady(pplx::cancellation_token);
+		std::function<void()> OnConnectedPlayerChanged(std::function<void(SessionPlayer, std::string)> callback);
+		std::function<void()> OnRoleReceived(std::function<void(std::string)> callback);
+		void OnTunnelOpened(std::function<void(std::shared_ptr<Stormancer::P2PTunnel>)> callback);
 
-		std::vector<SessionPlayer> getConnectedPlayers();
+		void OnP2PConnected(std::function<void(std::shared_ptr<Stormancer::P2PScenePeer>)> callback);
 
-		std::function<void()> onConnectedPlayerChanged(std::function<void(SessionPlayer)> callback);
+		void OnShutdownReceived(std::function<void(void)> callback);
 
 		template<typename TOut, typename TIn>
 		pplx::task<TOut> sendGameResults(TIn results)
@@ -79,26 +134,30 @@ namespace Stormancer
 
 		pplx::task<void> connect();
 		pplx::task<void> reset();
+		pplx::task<void> disconnect();
 
-		void ready();
+		bool shouldEstablishTunnel = true;
 
-#pragma endregion
+		Scene_ptr GetScene() {
+			return this->_scene;
+		}
+
+		void ready(std::string data);
+	private:
+		void unsubscribeConnectedPlayersChanged(Action<SessionPlayerUpdateArg>::TIterator handle);
+		void unsubscribeRoleReceived(Action<std::string>::TIterator handle);
 
 	private:
-
-#pragma region private_methods
-
-		void unsubscribeConnectedPlayersChanged(Action<SessionPlayer>::TIterator handle);
-
-#pragma endregion
-
-#pragma region private_members
-
-		Action<SessionPlayer> _onConnectedPlayersChanged;
+		std::shared_ptr<P2PTunnel> _tunnel;
+		Action<SessionPlayerUpdateArg> _onConnectedPlayersChanged;
+		Action<std::string> _onRoleReceived;
+		std::function<void(void)> _onShutdownReceived;
+		std::function<void(std::shared_ptr<Stormancer::P2PTunnel>)> _onTunnelOpened;
+		std::function<void(std::shared_ptr<Stormancer::P2PScenePeer>)> _onConnectionOpened;
 		Scene_ptr _scene;
 		std::vector<SessionPlayer> _users;
-		pplx::task_completion_event<GameServerInformations> _waitServerTce;
+		pplx::task_completion_event<std::string> _waitServerTce;
+		std::shared_ptr<Stormancer::ILogger> _logger;
 
-#pragma endregion
 	};
 }
