5a0957912a72be0df1dec3d2a2ce7210a72ff4c0
Stormancer-HAL9000 <hal9000@stormancer.com>
2019-02-14 11:02:34 +0100

    Origin: b02d6eac0d893c1fdc0e9563efc927b5b79c047a
    This is an automated commit.

--- scripts/BuildSwitch.bat
+++
@@ -1,3 +0,0 @@
-.nuget\nuget.exe restore stormancer-sdk-cpp-nx.sln
-msbuild stormancer-sdk-cpp-nx.sln /t:stormancer-sdk-cpp-nx /p:Configuration=Release /p:Platform=NX64
-msbuild stormancer-sdk-cpp-nx.sln /t:stormancer-sdk-cpp-nx /p:Configuration=Debug /p:Platform=NX64
