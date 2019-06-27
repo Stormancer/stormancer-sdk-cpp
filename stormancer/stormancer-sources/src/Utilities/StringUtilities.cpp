1f2852fe61b81b31276dbf210f653918b3754805
Stormancer-HAL9000 <hal9000@stormancer.com>
2019-06-27 18:18:45 +0200

    Origin: cfd94dd07ccb2303ccb16fd33578d6e8942c80d9
    This is an automated commit.

--- stormancer/stormancer-sources/src/Utilities/StringUtilities.cpp
+++ stormancer/stormancer-sources/src/Utilities/StringUtilities.cpp
@@ -1,12 +1,27 @@
 #include "stormancer/stdafx.h"
 #include "stormancer/Utilities/StringUtilities.h"
 #include <sstream>
+#include <string>
 
 namespace Stormancer
 {
-	std::string vectorJoin(const std::vector<std::string>& vector, const std::string& glue)
+	std::string stringJoin(const std::vector<std::string>& vector, const std::string& glue)
 	{
 		std::stringstream ss;
+		for (size_t i = 0; i < vector.size(); i++)
+		{
+			if (i != 0)
+			{
+				ss << glue;
+			}
+			ss << vector[i];
+		}
+		return ss.str();
+	}
+
+	std::wstring stringJoin(const std::vector<std::wstring>& vector, const std::wstring& glue)
+	{
+		std::wstringstream ss;
 		for (size_t i = 0; i < vector.size(); i++)
 		{
 			if (i != 0)
@@ -37,16 +52,7 @@ namespace Stormancer
 		return stringSplit(str, std::string(&separator, 1));
 	}
 
-	std::string stringTrim(const std::string& str2, char ch)
-	{
-		auto str = str2;
-		std::function<int(int)> ischar = [ch](int c) { return ((c == ch) ? 1 : 0); };
-		str.erase(str.begin(), find_if(str.begin(), str.end(), not1(ischar)));
-		str.erase(find_if(str.rbegin(), str.rend(), not1(ischar)).base(), str.end());
-		return str;
-	}
-
-	std::vector<std::wstring> wstringSplit(const std::wstring& str, const std::wstring& separator)
+	std::vector<std::wstring> stringSplit(const std::wstring& str, const std::wstring& separator)
 	{
 		std::vector<std::wstring> splitted;
 		size_t cursor = 0, lastCursor = 0, separatorSize = separator.size();
@@ -60,12 +66,21 @@ namespace Stormancer
 		return splitted;
 	}
 
-	std::vector<std::wstring> wstringSplit(const std::wstring& str, const wchar_t separator)
+	std::vector<std::wstring> stringSplit(const std::wstring& str, const wchar_t separator)
 	{
-		return wstringSplit(str, std::wstring(&separator, 1));
+		return stringSplit(str, std::wstring(&separator, 1));
 	}
 
-	std::wstring wstringTrim(const std::wstring& str2, wchar_t ch)
+	std::string stringTrim(const std::string& str2, char ch)
+	{
+		auto str = str2;
+		std::function<int(int)> ischar = [ch](int c) { return ((c == ch) ? 1 : 0); };
+		str.erase(str.begin(), find_if(str.begin(), str.end(), not1(ischar)));
+		str.erase(find_if(str.rbegin(), str.rend(), not1(ischar)).base(), str.end());
+		return str;
+	}
+
+	std::wstring stringTrim(const std::wstring& str2, wchar_t ch)
 	{
 		auto str = str2;
 		std::function<int(int)> ischar = [ch](int c) { return ((c == ch) ? 1 : 0); };
